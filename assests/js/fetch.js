30b243b1b8589a40d6fb38043e40c547077dc31a
Blaize9931 <blaize.kenny@outlook.com>
2026-02-05 20:36:48 +0000

    established fetch function

--- assests/js/fetch.js
+++ assests/js/fetch.js
@@ -1,7 +1,11 @@
 // Fetch data using Fetch API
-fetch(url)
+fetch('https://newsapi.org/v2/everything?q=tesla&from=2026-01-05&sortBy=publishedAt&apiKey=d199684dc0a64bfeb06f85cc8baa0aaf')
   .then((response) => response.json())
-  .then((data) => console.log(data))
-  .catch((error) => console.log(error));
+  .then((data) => displayNews(data))
+  .catch((error) => console.log(error)); 
+
+  function displayNews(articles) {
+    console.log(articles);
+  }
 
 
