9f918996c1f08c602af683b1e8d55a1920be1d23
JahidulSany <jahidulsanypro@gmail.com>
2026-02-07 20:35:22 +0000

    update the url

--- assests/js/fetch.js
+++ assests/js/fetch.js
@@ -1,5 +1,5 @@
 fetch(
-  'https://newsapi.org/v2/everything?q=tesla&from=2026-01-05&sortBy=publishedAt&apiKey=d199684dc0a64bfeb06f85cc8baa0aaf',
+  'https://newsapi.org/v2/top-headlines?country=us&apiKey=d199684dc0a64bfeb06f85cc8baa0aaf',
 )
   .then((response) => response.json())
   .then((data) => displayNews(data))
